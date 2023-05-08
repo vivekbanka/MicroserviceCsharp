1d39f66035a10c4a758b45bf59fad5ebb113ef20
vivek.banka <35413989+vivekbanka@users.noreply.github.com>
2023-05-08 15:49:28 -0400

    Added the Platform Service Controller.

---
+++ PlatformSerivces/PlatformSerivces/Controllers/PlatformServiceController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlatformSerivces.Data;
+using PlatformSerivces.DTOs;
+
+namespace PlatformSerivces.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformServiceController : ControllerBase
+    {
+        private readonly IPlatformRepo _platformRepo;
+        private readonly IMapper _mapper;
+
+        public PlatformServiceController(IPlatformRepo platformRepo, IMapper mapper)
+        {
+            _platformRepo = platformRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetRepositories()
+        {
+            var result = _platformRepo.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformDTO>>(result));
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetActionResult(int id)
+        {
+            var result = _platformRepo.GetPlatformById(id);
+            return Ok(_mapper.Map<PlatformDTO>(result));
+        }
+    }
+}
---
+++ PlatformSerivces/PlatformSerivces/Profiles/PlatformProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PlatformSerivces.DTOs;
+using PlatformSerivces.Models;
+
+namespace PlatformSerivces.Profiles
+{
+    public class PlatformProfile: Profile
+    {
+        public PlatformProfile()
+        {
+            // source to targer 
+
+            CreateMap<Platform, PlatformDTO>();
+            CreateMap<PlatformCreateDTO, Platform>();
+        }
+    }
+}
